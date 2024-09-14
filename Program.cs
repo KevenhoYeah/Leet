147563c08c28b1a48753e98ae5dfbe393dabf0a8
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-13 22:33:57 -0300

    Finalização de exercicio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine ("traduto em Leets");
+
+string Frase = Console.ReadLine();
+
+string FraseEmLeets = Frase
+
+.Replace("A", "4" )
+.Replace("e", "3" )
+;
+
+Console.WriteLine($"\n Tradução em leets:\n\n {FraseEmLeets}");
+
+
